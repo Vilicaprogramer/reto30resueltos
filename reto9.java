6d7fe905511f306661a48d5f4863180cbe027cb1
Vicen <150998620+Vilicaprogramer@users.noreply.github.com>
2025-04-12 19:24:14 +0200

    Reto 9 con Python

---
+++ reto9.java
@@ -0,0 +1,31 @@
+/* Los alumnos de Prometeo que cursan DAM/DAW + Master andan muy liados y no tienen claro cuando tienen clase y 
+cuando no. 
+
+Haz un programa que les indique si tienen clase de FP, de Máster, o de las dos en un conjunto de fechas dado. El 
+usuario introduce un número N, que indica el número de días que quiere ver, se deberá imprimir el número del día, 
+si no tienen nada en esa fecha, FP si tiene clase de un módulo de DAM/DAW o Máster si ese día tiene clase de máster. 
+Los días múltiplos de 3 tienen clase de FP.
+Los días múltiplos de 5, de máster.
+Los días que son múltiplos de 3 y 5 tienen clase de las dos.
+
+Ejemplo de entrada:
+---------------------------------
+15
+
+Ejemplo de salida:
+---------------------------------
+1
+2
+FP
+4
+Máster
+FP
+7
+8
+FP
+Máster
+11
+FP
+13
+14
+FP + Máster */
