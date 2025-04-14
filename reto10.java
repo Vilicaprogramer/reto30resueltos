6bdfaa7bead78ac76b264139855367c0155c5e5e
Vicen <150998620+Vilicaprogramer@users.noreply.github.com>
2025-04-14 15:54:45 +0200

    Reto 10 Java

--- reto10.java
+++ reto10.java
@@ -1,3 +1,4 @@
+
 /* 📌 10. RETO 30 DÍAS ABRIL: DIA 10 | COOKIES SUPER RICAS S.A-----------------------------------------
 Una tienda de galletas artesanales vende al por mayor en su página web. Cada caja de galletas cuesta 6€. Solicita 
 la cantidad de cajas de galletas en cada venta y dependiendo de la cantidad introducida se realizan los siguientes 
@@ -17,7 +18,63 @@ Total € de la compra.
 Gastos de envío (si los tiene o si es gratuito)
 Descuento por compra (Indicar al cliente cuántos € falta para entrar en una promoción si la compra es <100€ o el 
 importe de descuento generado si es superior)  */
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class reto10 {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int nmCajas;
+        int gastosEnvio = 0;
+        double precioTotal;
+        double promo;
+
+        while (true) {
+            try {
+                System.out.print("Introduce un número de cajas: ");
+                nmCajas = sc.nextInt();
+
+                if (nmCajas < 5) {
+                    System.out.println("No se permiten compras inferiores a 5 cajas de galletas");
+                    sc.nextLine();
+                    continue;
+                } else if (nmCajas >= 5 || nmCajas < 15) {
+                    gastosEnvio = 10;
+                    System.out.println("Número total de cajas a comprar: " + nmCajas);
+                    System.out.println("Gastos de envío: " + gastosEnvio + "€");
+                } else {
+                    System.out.println("Número total de cajas a comprar: " + nmCajas);
+                    System.out.println("Gastos de envío: " + gastosEnvio + "€");
+                }
+
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("El dato introducido no es un número, inténtalo de nuevo.");
+                sc.nextLine();
+            }
+        }
+        precioTotal = nmCajas * 6;
+        if (precioTotal >= 120 && precioTotal < 250) {
+            precioTotal = (precioTotal * 0.95) + gastosEnvio;
+            System.out.println("Total de la compra: " + String.format("%.2f", precioTotal) + "€");
+        } else if (precioTotal >= 250) {
+            precioTotal = (precioTotal * 0.90) + gastosEnvio;
+            System.out.println("Total de la compra: " + String.format("%.2f", precioTotal) + "€");
+        } else {
+            System.out.println("Total de la compra: " + String.format("%.2f", precioTotal) + "€");
+        }
+
+        if (precioTotal < 120) {
+            promo = 120 - precioTotal;
+            System.out.println(
+                    "Te faltan" + String.format("%.2f", promo) + " para llegar a una promoción del 5% de descuento");
+        } else if (precioTotal < 250) {
+            promo = 250 - precioTotal;
+            System.out.println(
+                    "Te faltan" + String.format("%.2f", promo) + " para llegar a una promoción del 10% de descuento");
+        } else {
+            System.out.println("Tienes el máximo de promociones");
+        }
+    }
 }
--- reto9.java
+++ reto9.java
@@ -35,7 +35,7 @@ import java.util.Scanner;
 
 public class reto9 {
     public static void main(String[] args) {
-        int fecha = 0;
+        int fecha;
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -54,7 +54,7 @@ public class reto9 {
                 sc.nextLine();
             }
         }
-        
+
         for (int i = 1; i <= fecha; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FP + Máster");
